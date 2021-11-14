e60a6f802be6dcc5d626e9cf158a891d16918daa
Kevin Raddatz <kevin.raddatz@snhu.edu>
2021-11-14 17:41:14 -0600

    Module 3 Commit

--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -1,8 +1,7 @@
 const express = require('express'); 
-const router = express.router(); 
+const router = express.router; 
 const controller = require('../controllers/travel'); 
 
 /* GET home page. */ 
-router.get('/', controller.travel); 
-
+router.get('/', controller.travel); //Throwing Error 11/14 
 module.exports = router; 
